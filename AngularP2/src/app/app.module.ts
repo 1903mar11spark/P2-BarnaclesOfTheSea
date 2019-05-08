c32e1e177c8658b7d0c6790245224100b6f2dabb
Em <ebaillie27@gmail.com>
2019-05-08 00:04:21 -0400

    Added more comps, services, methods
    Need to figure out layout a bit more, as login may can be small on welcome page, or whatever, will consult in morn

--- AngularP2/src/app/app.module.ts
+++ AngularP2/src/app/app.module.ts
@@ -1,13 +1,27 @@
+//MyModules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; //BootStrap import 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+//may add Material and Graphics 
+//as comments aren't permitted in json, to add Bootstrap you also have to go to the angular.json file and add
+// "node_modules/bootstrap/dist/css/bootstrap.min.css" this to the styles section among assets
 
+
+//Routing
 import { AppRoutingModule } from './app-routing.module';
+
+//Good old components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { InfoComponent } from './components/info/info.component';
 import { NavComponent } from './components/layout/nav/nav.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+
+//services 
 
 @NgModule({
   declarations: [
@@ -15,14 +29,19 @@ import { NavComponent } from './components/layout/nav/nav.component';
     LoginComponent,
     HomeComponent,
     InfoComponent,
-    NavComponent
+    NavComponent,
+    CreateUserComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
+  //lets ang know root component
   bootstrap: [AppComponent]
 })
 export class AppModule { }
